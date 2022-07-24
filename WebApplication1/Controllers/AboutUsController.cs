9627bb758822c8b7382bdb322766aff2e0427c2f
swatidalal <swati15091999@gmail.com>
2022-07-24 21:00:26 +0530

    Add project files.

---
+++ WebApplication1/Controllers/AboutUsController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class AboutUsController:Controller
+    {
+        private readonly ILogger<AboutUsController> _logger;
+        public AboutUsController(ILogger<AboutUsController> logger)
+        {
+            _logger = logger;
+        }
+        public IActionResult AboutUs()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
